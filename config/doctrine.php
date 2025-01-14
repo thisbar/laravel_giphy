f83c485396074bddc9f37bf3c73206327a30cdc6
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-14 12:48:31 -0300

    chore(doctrine): Add domain base path for entities

--- config/doctrine.php
+++ config/doctrine.php
@@ -30,7 +30,7 @@ return [
             'meta'          => env('DOCTRINE_METADATA', 'attributes'),
             'connection'    => env('DB_CONNECTION', 'mysql'),
             'paths'         => [
-                base_path('app/Entities')
+                base_path('src/Core/*/Domain')
             ],
 
             'repository'    => Doctrine\ORM\EntityRepository::class,

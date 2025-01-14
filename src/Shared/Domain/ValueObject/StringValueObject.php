ac1f04e67f7ecc8a6ac1dc8f40acaa0454351dc6
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-14 02:07:30 -0300

    feat(value objects): Add base shared value objects

---
+++ src/Shared/Domain/ValueObject/StringValueObject.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelGhipy\Shared\Domain\ValueObject;
+
+use InvalidArgumentException;
+
+abstract class StringValueObject
+{
+	public function __construct(protected readonly string $value) {}
+
+	public function value(): string
+	{
+		return $this->value;
+	}
+
+	protected function ensureLengthIsBetweenRange(string $value, int $min, int $max): void
+	{
+		$valueLength = strlen($value);
+
+		if ($valueLength < $min || $valueLength > $max) {
+			throw new InvalidArgumentException(sprintf(
+				'The value length must have between <%d> and <%d> characters. Value: <%s>',
+				$min,
+				$max,
+				$value
+			));
+		}
+	}
+}

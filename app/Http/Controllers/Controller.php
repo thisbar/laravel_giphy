c2dfff95f817d9013525edfb7822c0a4edad6b93
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-16 16:16:52 -0300

    feat(controller): Add json response methods to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function successResponse(array $data): JsonResponse
+    {
+        return response()->json($data);
+    }
+
+    protected function errorResponse(string $message, int $statusCode): JsonResponse
+    {
+        return response()->json(['error' => $message], $statusCode);
+    }
 }

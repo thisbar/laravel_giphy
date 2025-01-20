4a69ccfe946694ac5677775f1fddb05102967a4c
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-20 05:37:39 -0300

    fix(auth): Fix issues with auth exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +28,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception): JsonResponse
+    {
+        return response()->json([
+            'error' => $exception->getMessage() ?: 'Unauthorized',
+        ], 401);
     }
 }
--- src/Core/Users/Infrastructure/PassportUserAdapter.php
+++ src/Core/Users/Infrastructure/PassportUserAdapter.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace LaravelGhipy\Core\Users\Infrastructure;
 
+use Exception;
 use Illuminate\Contracts\Auth\Authenticatable;
 use LaravelGhipy\Core\Users\Domain\User;
 
 final class PassportUserAdapter implements Authenticatable
 {
 	public readonly User $user;
+	private string $accessToken = '';
 
 	public function __construct(User $user)
 	{
@@ -34,6 +36,27 @@ final class PassportUserAdapter implements Authenticatable
 	public function getRememberToken(): null
 	{
 		return null;
+	}
+
+	public function __get(string $name)
+	{
+		if ($name === 'id') {
+			return $this->getAuthIdentifier();
+		}
+
+		throw new Exception('Undefined property: ' . self::class . "::\$$name");
+	}
+
+	public function withAccessToken(string $token): self
+	{
+		$this->accessToken = $token;
+
+		return $this;
+	}
+
+	public function getAccessToken(): ?string
+	{
+		return $this->accessToken;
 	}
 
 	public function setRememberToken($value) {}

4a2dcc2817c249f0a69d4ad76b07d475ec94c130
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-19 23:52:28 -0300

    chore(config): Set domain user and doctrine as auth provider

--- config/auth.php
+++ config/auth.php
@@ -61,8 +61,8 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'driver' => 'doctrine',
+            'model' => LaravelGhipy\Core\Users\Domain\User::class,
         ],
 
         // 'users' => [

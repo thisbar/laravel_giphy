be1d448fced9124d1f41764d57e9da3680f437b4
Lautaro W. Aguirre <scrixsys@gmail.com>
2025-01-21 13:48:03 -0300

    chore(elastic): Fix mapping types issues and loading data

--- etc/docker/elastic/kibana_load_objects.sh
+++ etc/docker/elastic/kibana_load_objects.sh
@@ -6,12 +6,28 @@ until curl -s "$KIBANA_HOST/api/status" > /dev/null; do
   sleep 5
 done
 
-if [ -f /data/saved_objects/kibana.ndjson ]; then
+sleep 15
+
+if [ -f /scripts/saved_objects/kibana.ndjson ]; then
   echo "Importando objetos guardados..."
-  curl -X POST "$KIBANA_HOST/api/saved_objects/_import?overwrite=true" \
-    -H 'kbn-xsrf: true' \
-    --form file=@/data/saved_objects/kibana.ndjson
-  echo "Importación completada."
+
+  max_attempts=5
+  attempt=1
+  while [ $attempt -le $max_attempts ]; do
+    response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$KIBANA_HOST/api/saved_objects/_import?overwrite=true" \
+      -H 'kbn-xsrf: true' \
+      --form file=@/scripts/saved_objects/kibana.ndjson)
+
+    if [ "$response" = "200" ] || [ "$response" = "409" ]; then
+      echo "Importación completada con código $response."
+      break
+    else
+      echo "Error en la importación (código $response). Reintentando..."
+      sleep 5
+    fi
+    attempt=$((attempt+1))
+  done
+
 else
   echo "No se encontraron objetos guardados para importar."
 fi
